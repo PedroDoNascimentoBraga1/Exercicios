6464520cd5465e59138520473cd28ca714bbe763
pedro <pedro.do.nascimento.braga@gmail.com>
2025-01-19 23:52:27 -0300

    edição do ordenacaonumerica.py

--- ordenacaonumerica.py
+++ ordenacaonumerica.py
@@ -7,7 +7,7 @@ numeral = list(input('Informe um numeral: '))
 for cont in range (len(numeral)): numeral[cont] = int(numeral[cont])
 
 for cont0 in range (len(numeral)):
-    for cont1 in range (len(numeral)):
-        if numeral[cont0] < numeral [cont1]:
-            numeral[cont0] , numeral [cont1] =  numeral [cont1] , numeral[cont0]
+    for cont in range (len(numeral)):
+        if numeral[cont0] < numeral [cont]:
+            numeral[cont0] , numeral [cont] =  numeral [cont] , numeral[cont0]
 print(numeral)
